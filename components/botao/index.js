b5cfd73716e3043523aba5eefded04ae1c3761ac
costaemily <costa.emily@outlook.com>
2022-12-09 13:16:31 -0300

    deletando o MySQL e fazendo a conexão Firebase

--- components/botao/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View } from 'react-native';
-
-import estilos from './botoes';
-
-export default function Botao() {
-    return <View style={estilos.botao} />
-}
---
+++ config/connectionFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBWRkZ20ZKqZUSEGvakQ_uRDx8klF5m78o",
+  authDomain: "spis-project.firebaseapp.com",
+  projectId: "spis-project",
+  storageBucket: "spis-project.appspot.com",
+  messagingSenderId: "561134350228",
+  appId: "1:561134350228:web:fbfebe7f697ab03d216981"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
