b46fa13d7bd15a0604006fb9806e35d0b6490422
costaemily <costa.emily@outlook.com>
2022-12-09 02:29:38 -0300

    tela inicial front

---
+++ components/botao/botoes.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    botao: {
+        backgroundColor: 'black',
+        width: 100,
+        height: 200,
+        
+    }
+  });
---
+++ components/botao/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import estilos from './botoes';
+
+export default function Botao() {
+    return <View style={estilos.botao} />
+}
